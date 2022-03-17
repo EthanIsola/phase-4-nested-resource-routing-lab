97ad38491e90562e7c7e07681fc736370f4cf1f7
Ethan Isola <isolaethan@gmail.com>
2022-03-17 15:12:01 -0500

    commit tree 86d371f9c4a31b6c6a2196b4decb4d92474b439a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      items = Item.where(user_id: params[:user_id])
+      if items === []
+        render json: { error: "User not found" }, status: :not_found
+      else
+        render json: items
+      end
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
   end
 
+  def show
+    if params[:user_id]
+      items = Item.find(params[:id])
+      if items === []
+        render json: { error: "User not found" }, status: :not_found
+      else
+        render json: items
+      end
+    else
+      items = Item.find(params[:id])
+      render json: items, include: :user
+    end
+  end
+
+  def create
+    item = Item.create!(name: params[:name], description: params[:description], price: params[:price], user_id: params[:user_id])
+    render json: item, status: :created
+  end
 end
